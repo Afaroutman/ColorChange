c722d386930e597097570d3bf102355297d73401
Afaroutman <ahit0637@740s-pl-112.local>
2015-12-17 08:46:51 -0700

    Changes colors

--- ColorChange/ViewController.swift
+++ ColorChange/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func RandomRed(sender: UIButton)
+    {
+        let newRed = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+         view.backgroundColor = UIColor(red: newRed, green: 0, blue: 0, alpha:1.0)
+    }
+    @IBAction func RandomGreen(sender: UIButton)
+    {
+        let newGreen = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+         view.backgroundColor = UIColor(red: 0, green: newGreen, blue: 0, alpha:1.0)
+    }
+    @IBAction func RandomBlue(sender: UIButton)
+    {
+        let newBlue = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+        view.backgroundColor = UIColor(red: 0, green: 0, blue: newBlue, alpha:1.0)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
